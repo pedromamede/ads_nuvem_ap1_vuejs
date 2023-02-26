32108ab00adfb89f613a70024e2aa85c4d01ea1d
Pedro Mamede <pedro.mamede@gmail.com>
2023-02-26 23:16:06 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import MinhaApp from './components/MinhaApp.vue'
+</script>
+
+<template>
+  <header>
+    <h1> Unifor - ADS </h1>
+    <h3> Desenvolvimento de software em nuvem</h3>
+
+    <div class="wrapper">
+      <MinhaApp msg="Deu certo!" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/MinhaApp.vue
@@ -0,0 +1,58 @@
+<script>
+export default {
+  data() {
+  
+    var numeros = [
+      (Math.floor(Math.random() * (99))),
+      (Math.floor(Math.random() * (99))),
+      (Math.floor(Math.random() * (99))),
+      (Math.floor(Math.random() * (99))),
+      (Math.floor(Math.random() * (99)))
+    ]
+    var maior = Math.max.apply(0, numeros);
+    var menor = Math.min.apply(0, numeros);
+ 
+    
+    return {
+      numeros: numeros,
+      maior: maior,
+      menor:menor
+    }
+  }
+}
+</script>
+
+<template>
+  <br/><br/>
+  <div>
+    <h5> Essa aplicação gera 5 números aleatórios, apresenta-os e informa o maior e menor número entre eles </h5> 
+    
+    <h5>Numeros: {{numeros}}</h5>
+    <h5>Maior: {{maior}}</h5>
+    <h5>Menor: {{menor}}</h5>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
